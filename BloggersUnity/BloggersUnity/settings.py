4f195221f552ae6f00fde2da799a3964b5059e59
unknown <prakashsatvi00@gmail.com>
2023-11-06 13:32:51 +0530

    update code for Railway deployment

--- BloggersUnity/BloggersUnity/settings.py
+++ BloggersUnity/BloggersUnity/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-ij7rq-ji^xkq4oy@96m+e0&w&thkf-q$%6_j0z9c$)pnl&84%x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -112,7 +112,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'Tech', 'media')
 
 # Static files settings
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'build_files', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
